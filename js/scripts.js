2716bb1a5e44748540cb4b627032f6991ebf5331
Kairos <hestiatay@gmail.com>
2023-05-07 11:57:13 +0800

    Updated certifications.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,43 @@
 $(document).ready(function(){
-  $('#certificate-gallery1, #certificate-gallery2, #certificate-gallery3').slick({
-    dots: true, // Show the navigation dots
-    arrows: true, // Show the navigation arrows
-    infinite: true, // Enable infinite scrolling
-    speed: 300, // Animation speed in milliseconds
-    slidesToShow: 1, // Number of images to show at once
-    adaptiveHeight: true // Set the height of the container to the height of the current slide
+  $('#certificate-gallery1').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
+$(document).ready(function(){
+  $('#certificate-gallery2').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
+$(document).ready(function(){
+  $('#certificate-gallery3').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
+$(document).ready(function(){
+  $('#certificate-gallery4').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
   });
 });
